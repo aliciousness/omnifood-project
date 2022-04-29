191857d84487c7d394a0ba140e4da7fbb84d5a7e
Richard <craddockrichard09@gmail.com>
2022-04-28 22:09:33 -0400

    added readme file, update buildspec file and delete lambda

---
+++ README.md
@@ -0,0 +1,5 @@
+#OMNIFOODSOLUTIONS.COM
+
+This is a website I designed in a [Udemy](https://www.udemy.com/) class with Jonas Schmedtmann's [HTML and Css class](https://www.udemy.com/course/design-and-develop-a-killer-website-with-html5-and-css3/). I used this website to practice my skills with hosting a website.
+
+For buildspec file, S3_BUCKET is set to main bucket in environment on build project. If you would like to change buckets, one would need to change bucket location in environment variable
--- lambda.py
+++ lambda.py
@@ -1,41 +1,7 @@
-# import os
 
-# path = 'c:/Python39/lib/os.py'
-# #tpyeError: expected str, bytes or os.pathlike obj, not dict. This path does not work in powershell 
-
-# for root, directories, files in os.walk(path, topdown=False):
-# 	for name in files:
-# 		print(os.path.join(root, name))
-# 	for name in directories:
-# 		print(os.path.join(root, name))
-#----------------------------------------------------------------------------------------------------------------------------------------
-# import boto3 
-# zip = client.get_object(Bucket='richnet-website',Key='myBuild.zip') 
-# for f in zip:
-#     print(f)
-      
+  #this script unzips files from one bucket and sends all files to another bucket using lambda  
 
 '''
-  import boto3
-  
-  client = boto3.client('s3')
-  client.list_buckets()
-  
-  all s3 buckets come up, need to figure out the path for the dir for the s3 bbuckets on this client
-  
-  i think the path for the buckets maybe C:/Python39/lib/os.py, nvm not a dir
-  
-  ** import boto3 
-  **zip = client.get_object(Bucket='richnet-websiite',Key='myBuild.zip') 
-  **for f in zip:
-  **    print(f)
-  this script prints all the names of the files in this bucket. do i know if its zipped, no idea
-'''
-  
-  
-  # -----------------------------------------------------------------------------------------------------------------------------------------------
-  
-  
 import zipfile,boto3,os
 from io import BytesIO
 
@@ -62,6 +28,8 @@ path = '/tmp'
 for root,dirs,files in os.walk(path):
   for name in files:
     s3.meta.client.upload_file(os.path.join(root, name),'unzipped-richnet-website',name)
+    
+'''
 
 # on windows IMPORTANT
 # for root, dirs, files in os.walk(path):
@@ -71,27 +39,13 @@ for root,dirs,files in os.walk(path):
 #     s3.meta.client.upload_file(old,'unzipped-richnet-website',key)
   
   
-# for name in direct               NOTE dont need all this, not sure why i put this in this if i walk through files, it will walk through all dirs and sub dirs to all files in the directory path i choose. 
-#   old = r'{}'.format(os.path.join(root, name))  #didnt try this yet 
-#   newPath = old.replace(os.sep,'/')
-#   s3.meta.client.upload_file(f'{newPath}','unzipped-richnet-website',f'{name}')
 
-
-# import os
-
-# path = r"C:\temp\myFolder\example"
-
-# newPath = path.replace(os.sep, '/')
-
-# print(newPath)
+  
 
 
 
-import json
 
-def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+
+
+
+
